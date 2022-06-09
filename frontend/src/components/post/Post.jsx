fb3dc8e38466fcc2afb638ef330d46aa03a6e55b
Groot-2001 <shivasilmawala841@gmail.com>
2022-06-09 17:24:09 +0530

    feedbar and rightbar added

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -1,9 +1,38 @@
+import { MoreVert } from "@material-ui/icons";
 import "./post.css";
+import {user} from "../../dummyData";
 
-export default function Post() {
+export default function Post({post}) {
+  const users = user.filter(u=>u.id===1);
+  console.log(users);
   return (
-    <div>
-      
+    <div className="post">
+      <div className="post-wrapper">
+        <div className="post-top">
+          <div className="post-top-left">
+            <img src={user.filter(u=>u.id===post.userId)[0].profilepicture} alt="" className="post-top-img" />
+            <span className="post-top-left-username">{user.filter(u=>u.id===post.userId)[0].username}</span>
+            <span className="post-date">1 hr ago</span>
+          </div>
+          <div className="post-top-right">
+            <MoreVert/>
+          </div>
+        </div>
+        <div className="post-center">
+          <span className="post-description">{post?.desc}</span>
+          <img src={post.photo} alt="" className="post-img" />
+        </div>
+        <div className="post-bottom">
+          <div className="post-bottom-left">
+            <img src="/assets/like.png" alt="" className="post-reaction" />
+            <img src="/assets/heart.png" alt="" className="post-reaction" />
+            <span className="post-reaction-counter">{post.like} people liked it</span>
+          </div>
+          <div className="post-bottom-right">
+            <span className="post-comment-text">{post.comment} comments</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/components/rightbar/Rightbar.jsx
+++ frontend/src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,53 @@ import "./rightbar.css"
 export default function Righbar() {
   return (
     <div className="rightbar">
-      Rightbar
+      <div className="rightbar-wrapper">
+        <div className="birthday-container">
+          <img src="/assets/gift.png" alt="" className="birthday-img" />
+          <span className="birthday-text">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birthday today!
+          </span>
+        </div>
+        <img src="/assets/ad.avif" alt="" className="rightbar-ad" />
+      </div>
+      <h4 className="rightbar-title">Online Friends</h4>
+      <ul className="rightbar-friends-list">
+        <li className="rightbar-friends">
+          <div className="rightbar-img-container">
+            <img src="/assets/person/2.jpeg" alt="" className="friends-img" />
+            <span className="friends-online"></span>
+          </div>
+          <span className="username">Jhonny sins</span>
+        </li>
+        <li className="rightbar-friends">
+          <div className="rightbar-img-container">
+            <img src="/assets/person/2.jpeg" alt="" className="friends-img" />
+            <span className="friends-online"></span>
+          </div>
+          <span className="username">Jhonny sins</span>
+        </li>
+        <li className="rightbar-friends">
+          <div className="rightbar-img-container">
+            <img src="/assets/person/2.jpeg" alt="" className="friends-img" />
+            <span className="friends-online"></span>
+          </div>
+          <span className="username">Jhonny sins</span>
+        </li>
+        <li className="rightbar-friends">
+          <div className="rightbar-img-container">
+            <img src="/assets/person/2.jpeg" alt="" className="friends-img" />
+            <span className="friends-online"></span>
+          </div>
+          <span className="username">Jhonny sins</span>
+        </li>
+        <li className="rightbar-friends">
+          <div className="rightbar-img-container">
+            <img src="/assets/person/2.jpeg" alt="" className="friends-img" />
+            <span className="friends-online"></span>
+          </div>
+          <span className="username">Jhonny sins</span>
+        </li>
+      </ul>
     </div>
   )
 }
