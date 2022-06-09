d3f8d65c90e6f7749a68e5fa29be786808f490c6
Groot-2001 <shivasilmawala841@gmail.com>
2022-06-09 17:25:00 +0530

    dummyData added

---
+++ frontend/src/dummyData.js
@@ -0,0 +1,70 @@
+export const user = [
+    {
+        id:1,
+        profilepicture:"/assets/person/1.jpeg",
+        username:"Shiva Silmawala"
+    },
+    {
+        id:2,
+        profilepicture:"/assets/person/2.jpeg",
+        username:"Rocky Raja"
+    },
+    {
+        id:3,
+        profilepicture:"/assets/person/3.jpeg",
+        username:"Rohit jhunjhunwala"
+    },
+    {
+        id:4,
+        profilepicture:"/assets/person/4.jpeg",
+        username:"karan Teja"
+    },
+    {
+        id:5,
+        profilepicture:"/assets/person/5.jpeg",
+        username:"pappu Ahuja"
+    }
+];
+
+export const Posts =[
+    {
+        id:1,
+        desc:"hey good morning to all",
+        photo:"/assets/post/1.jpeg",
+        userId:1,
+        like:32,
+        comment:9
+    },
+    {
+        id:2,
+        desc:"Love for all,hatred for none.",
+        photo:"/assets/post/2.jpeg",
+        userId:2,
+        like:25,
+        comment:13
+    },
+    {
+        id:3,
+        desc:"god is great ,the greatest god,i am greatful",
+        photo:"/assets/post/3.jpeg",
+        userId:3,
+        like:39,
+        comment:15
+    },
+    {
+        id:4,
+        desc:"good vibes only",
+        photo:"/assets/post/4.jpeg",
+        userId:4,
+        like:42,
+        comment:11
+    },
+    {
+        id:5,
+        desc:"Everyday is a new opportunity",
+        photo:"/assets/post/5.jpeg",
+        userId:5,
+        like:51,
+        comment:23
+    }
+];
