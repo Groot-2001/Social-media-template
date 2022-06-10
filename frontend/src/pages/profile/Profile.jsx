2312f626b9b0da84e6e7daa5e9668bb4a9049150
Groot-2001 <shivasilmawala841@gmail.com>
2022-06-10 19:18:23 +0530

    Profile page added

---
+++ frontend/src/pages/profile/Profile.jsx
@@ -0,0 +1,32 @@
+import "./profile.css";
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+
+export default function Profile() {
+  return (
+    <>
+    <Topbar/>
+    <div className="profile">
+      <Sidebar/>
+      <div className="profile-right">
+            <div className="profile-right-top">
+                <div className="profile-cover">
+                    <img src="/assets/post/1.jpeg" alt="" className="profile-cover-img" />
+                    <img src="/assets/person/5.jpeg" alt="" className="profile-user-img" />
+                </div>
+                <div className="profile-info">
+                    <div className="profile-info-username"><b>Shiva Silmawala</b> </div>
+                    <div className="profile-info-desc">Busy at work</div>
+                </div>
+            </div>
+                <div className="profile-right-bottom">
+                    <Feed/>
+                    <Rightbar profile/>
+                </div>
+           </div>
+    </div>
+    </>
+  )
+}
